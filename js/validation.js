d852b1cfbd01d8cfad7d95c5965ffa3492c6e948
Johnny0n <janek.szarwaryn@gmail.com>
2021-10-04 14:24:52 +0200

    front-end task

---
+++ js/validation.js
@@ -0,0 +1,11 @@
+/*
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Yax | Software House Frontend Task - Javascript
+
+    W tym zadaniu postaraj się zaimplementować metody, które sprawdzą, czy dane wprowadzone
+    do formularza są poprawne. Przykładowo: czy imię i nazwisko zostało wprowadzone.
+    Możesz rozwinąć walidację danych tak bardzo, jak tylko zapragniesz.
+
+    Powodzenia :)
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
